0eefbd2761e80da1d8524ec33be9ea55e09d9060
ViraMesha <wira.mescha@gmail.com>
2022-11-06 21:46:35 +0200

    add script for all tasks

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,28 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+const LOCALSTORAGE_KEY = "videoplayer-current-time";
+const currentTime = Number(localStorage.getItem(LOCALSTORAGE_KEY)) || 0;
+
+player.on('timeupdate', throttle(onUpdate, 1000));
+
+function onUpdate({seconds}) {
+    console.dir(seconds)
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(seconds))
+}
+   
+player.setCurrentTime(currentTime).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,42 @@
+import throttle from 'lodash.throttle';
+
+const form = document.querySelector('.feedback-form');
+const LOCALSTORAGE_KEY = "feedback-form-state";
+let data = {};
+onLoad()
+form.addEventListener('input', throttle(onFormInput, 500));
+form.addEventListener('submit', onFormSubmit);
+
+
+function onFormInput (event) {
+ data = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)) || {};
+
+  data[event.target.name] = event.target.value;
+
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(data));
+}
+
+function onFormSubmit(event) {
+    event.preventDefault()
+    const { email, message } = event.currentTarget.elements;
+
+    const formOutput = { email: email.value, message: message.value };
+   
+    
+    if (email.value && message.value) {
+        console.log(formOutput);
+        form.reset();
+        localStorage.clear()
+    } else alert("Всі поля повинні бути заповнені!!!");
+}
+
+function onLoad() {
+    const onReloadStorage = JSON.parse( localStorage.getItem(LOCALSTORAGE_KEY))
+    if (!onReloadStorage) {
+      return;
+    }
+    data = onReloadStorage;
+    form.email.value = data.email || '';
+    form.message.value = data.message || '';
+}
 
